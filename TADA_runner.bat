b277258253ef9ea5397cbc741b632188583ff433
CHEN Wang <wang.chen@faurecia.com>
2021-02-15 11:29:09 +0100

    nothing changed

--- TADA_runner.bat
+++ TADA_runner.bat
@@ -1,13 +1,18 @@
 @echo off
 
-set ser=http://10.247.2.29:8501/
-set backup_ser=http://10.247.2.29:8502/
+set ser=http://10.61.180.168:8501/
+set backup_ser=http://10.247.15.68:8501/
+
+echo opening TADA with google chrome, please make sure you have chrome installed.
+echo Searching for valid server link, please wait... 
+echo #############################################################
+echo if TADA server is down, please contact wang.chen@faurecia.com
 
 curl %ser%
 if ErrorLevel 1 (
 	echo Failure
-	start "" %backup_ser%
+	start chrome %backup_ser%
 ) else (
 	echo Success
-	start "" %ser%
+	start chrome %ser%
 )
